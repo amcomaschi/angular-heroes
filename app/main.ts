8e0c4256d7533872d7703e516c420bdda2ffd95e
amcomaschi <arielcomaschi@gmail.com>
2016-07-15 17:18:36 -0300

    Initial commit

---
+++ app/main.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by amcomaschi on 15/07/16.
+ */
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+
+import { AppComponent } from './app.component';
+
+bootstrap(AppComponent);
